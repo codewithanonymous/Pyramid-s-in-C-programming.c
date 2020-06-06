51ce5009bbd3cf7d62e1ad78725071db005874e7
Code With Anonymous <65646201+codewithanonymous@users.noreply.github.com>
2020-06-06 16:23:31 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,27 @@
+#include<conio.h>
+int main(void)
+{
+	int i,j,n;
+	printf("Enter No. of lines :");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		printf(" ");
+		
+		 for(j=1;j<=2*i-1;j++)
+		 
+		printf("*");
+		printf("\n");
+	}
+	n--;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		printf(" ");
+		for(j=1;j<=2*(n-i)+1;j++)
+		printf("*");
+		printf("\n");
+	}
+	return 0;
+}
